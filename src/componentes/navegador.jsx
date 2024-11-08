c8a2186e3ce9e1ea59e129b8c3d07db9a2dfb67d
Andres David Aguirre Rengifo <andredavidrengifo@gmail.com>
2024-11-08 17:22:57 -0500

    navegador-prueba

---
+++ src/componentes/navegador.jsx
@@ -0,0 +1,35 @@
+import './navegador.css'
+import logo from '../img/logo.png'
+import { useState } from 'react'
+
+
+export const Navegador = () =>{
+    const [menu, setMenu ] = useState(false)
+    const toggleMenu = () =>{
+        setMenu(!menu)
+    }
+
+
+    return(
+        <>
+            <header className='navegador'>
+                <img className='logo' src={logo}></img>
+                <button onClick={toggleMenu}
+                    className='navegador-button'>
+                    <svg className='svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </button>
+                <nav className={ `navegador-nav ${ menu ? ' isActive' : ''}`}>
+                    <ul className="navegador-ul">
+                        <li className="navegador-li"><a href="" className="navegador-a">item #</a></li>
+                        <li className="navegador-li"><a href="" className="navegador-a">item #</a></li>
+                        <li className="navegador-li"><a href="" className="navegador-a">item #</a></li>
+                        <li className="navegador-li"><a href="" className="navegador-a">item #</a></li>
+                        <li className="navegador-li"><a href="" className="navegador-a">item #</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
