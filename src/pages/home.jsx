d3de8151fadb97ed869d3abfcc1204bedaf03478
Andres David Aguirre Rengifo <andredavidrengifo@gmail.com>
2024-11-10 20:49:02 -0500

    inicio-secion-base

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bienvenido a la Página de Inicio</h1>
+        </div>
+    );
+};
+
+export default HomePage;
