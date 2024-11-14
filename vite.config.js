dfd5ef86edb7874adb71cb2319455b7c44e8dbf6
Andres David Aguirre Rengifo <andredavidrengifo@gmail.com>
2024-11-13 22:42:53 -0500

    Cambio de configuraciones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://DavidmisoYT.github.io/web-institucional",
+  base: "https://davidmisoyt.github.io/web-institucional",
 })
